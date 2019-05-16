328c7b635c0c474bd4e6e3fb5bf2330445797ce8
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-15 19:46:39 -0500

    Added collider trigger to static enemy along with restart game functionality when user is in enemies line of sight

---
+++ 3D Beginner/Assets/Scripts/Observer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    bool m_isPlayerInRange;
+    public GameEnding gameEnding;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_isPlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_isPlayerInRange)
+        {
+            // Check whether there are any Colliders along the path of a line
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+}
